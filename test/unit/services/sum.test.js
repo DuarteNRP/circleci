dc79f0a8554111e16538f76556dae78ce2d4f230
DuarteNRP <duarte_pereira_5@hotmail.com>
2020-11-11 10:08:37 +0000

    next test

--- test/unit/services/sum.test.js
+++ test/unit/services/sum.test.js
@@ -4,7 +4,13 @@ describe('Main Service', () => {
   });
 });
 
-describe('Test PR', () => {
+describe('Test PR just had do it', () => {
+  it('should pass', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
+
+describe('Test PR 2', () => {
   it('should pass', () => {
     expect(1 + 1).toEqual(2);
   });
